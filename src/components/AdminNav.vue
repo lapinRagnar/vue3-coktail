333e2997456eddd6d90df9ead83e5cfb5e5886f7
lapinRagnar <lapinragnar@gmail.com>
2022-08-14 21:38:47 +0200

    ajouter des routerLink dans la nav admin left

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -1,9 +1,15 @@
 <template>
   <div>Admin nav marche
     <ul>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
+        <li>
+          <router-link to="/admin/users/index">liste des users</router-link>
+        </li>
+        <li>
+          <router-link to="/admin/user/add">Ajouter users</router-link>
+        </li>
+        <li>
+          <router-link to="/admin/user/edit/125">editer user 125</router-link>
+        </li>
     </ul>
   </div>
 </template>
