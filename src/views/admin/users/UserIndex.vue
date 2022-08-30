aeed7320d0c28faae190f7a5173a3462d72ba3b4
lapinRagnar <lapinragnar@gmail.com>
2022-08-30 19:20:44 +0200

    7 - afficher la listes des utilisateurs dans UserIndex

--- src/views/admin/users/UserIndex.vue
+++ src/views/admin/users/UserIndex.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    user index
+    <h1>Liste des utilisateurs</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>nom</th>
+          <th>prenom</th>
+          <th>email</th>
+          <th>date de création</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{user.id}}</td>
+          <td>{{user.nom}}</td>
+          <td>{{user.prenom}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.createdAt}}</td>
+        </tr>
+      </tbody>
+    </table>
     
   </div>
 </template>
@@ -10,9 +30,17 @@
   import { userService } from '@/_services'
 export default {
   name: 'UserIndex',
+  data() {
+    return {
+      users: []
+    }
+  },
   mounted(){
     userService.getAllUsers()
-     .then(res => console.log(res.data.data))
+     .then(res => {
+      console.log(res.data.data)
+      this.users = res.data.data
+     })
      .then(err => console.log(err))
   }
 }
