3d6188be97da0cf84785205936477da961a0b1b3
lapinRagnar <lapinragnar@gmail.com>
2022-08-30 18:18:55 +0200

    6 - gestion de session expirée

--- src/_services/caller.service.js
+++ src/_services/caller.service.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { accountService } from '@/_services'
+import router from '@/router'
 
 const Axios = axios.create({
   // baseURL: 'https://dummyjson.com'
@@ -7,7 +8,9 @@ const Axios = axios.create({
   baseURL: 'http://localhost:8989'
 })
 
-// permet d'ajouter le token dans l'entete headers Authorization
+/**
+ * permet d'ajouter le token dans l'entete headers Authorization
+ */
 Axios.interceptors.request.use(request => {
   
   console.log('je suis dans caller.service', request)
@@ -17,6 +20,20 @@ Axios.interceptors.request.use(request => {
   }
 
   return request
+})
+
+/**
+ * interceptor des reponses de l'api
+ */
+Axios.interceptors.response.use(response => {
+
+  return response
+}, error => {
+  console.log(error.response.status)
+  if (error.response.status == 401) {
+    accountService.logout()
+    router.push('/login')
+  }
 })
 
 export default Axios
